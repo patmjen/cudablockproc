f011fdcc3d7f300619c27765725cd94e3c9b5592
Patrick M. Jensen <patmjen@gmail.com>
2018-08-21 23:51:44 +0200

    Improve GetMemLocation test

--- src/test_util.cu
+++ src/test_util.cu
@@ -7,7 +7,6 @@ CUDA_TEST(UtilTest, GetMemLocation)
 {
     int x;
     void *hn_ptr, *hp_ptr, *d_ptr;
-    cudaError_t err;
 
     // Test normal host memory
     hn_ptr = &x;
@@ -22,18 +21,14 @@ CUDA_TEST(UtilTest, GetMemLocation)
     free(hn_ptr);
 
     // Test pinned host memory
-    err = cudaMallocHost(&hp_ptr, 1);
-    ASSERT_EQ(cudaSuccess, err) << "Could not allocate pinned host memory.";
+    ASSERT_EQ(cudaSuccess, cudaMallocHost(&hp_ptr, 1)) << "Could not allocate pinned host memory.";
     EXPECT_EQ(HOST_PINNED, getMemLocation(hp_ptr));
     EXPECT_TRUE(memLocationIs<HOST_PINNED>(hp_ptr));
-    err = cudaFreeHost(hp_ptr);
-    ASSERT_EQ(cudaSuccess, err) << "Could not free pinned host memory";
+    ASSERT_EQ(cudaSuccess, cudaFreeHost(hp_ptr)) << "Could not free pinned host memory";
 
     // Test device memory
-    err = cudaMalloc(&d_ptr, 1);
-    ASSERT_EQ(cudaSuccess, err) << "Could not allocate device memory";
+    ASSERT_EQ(cudaSuccess, cudaMalloc(&d_ptr, 1)) << "Could not allocate device memory";
     EXPECT_EQ(DEVICE, getMemLocation(d_ptr));
     EXPECT_TRUE(memLocationIs<DEVICE>(d_ptr));
-    err = cudaFree(d_ptr);
-    ASSERT_EQ(cudaSuccess, err) << "Could not free device memory";
+    ASSERT_EQ(cudaSuccess, cudaFree(d_ptr)) << "Could not free device memory";
 }

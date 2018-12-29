ea3a79f6160b51df146a2af72e22e6c0a4c8d0e4
Patrick M. Jensen <patmjen@gmail.com>
2018-12-29 13:19:00 +0100

    Add small benchmark kernel to main bench

--- src/main.cu
+++ src/main.cu
@@ -3,6 +3,38 @@
 #include "gtest/gtest.h"
 #include "cudablockproc.cuh"
 
+// TODO: Merge this with the version in util.cuh
+inline size_t gridLineBlocks(size_t nthr, const unsigned int siz)
+{
+    return siz/nthr + ((siz % nthr != 0) ? 1 : 0);
+}
+
+inline dim3 gridBlocks(const dim3 thrConfig, const int3 siz)
+{
+    return dim3(
+        gridLineBlocks(thrConfig.x, siz.x),
+        gridLineBlocks(thrConfig.y, siz.y),
+        gridLineBlocks(thrConfig.z, siz.z)
+    );
+}
+
+__global__ void sum3x3x3Kernel(float *out, const float *in, const int3 size)
+{
+    const int3 pos = make_int3(threadIdx);
+    if (pos >= 0 && pos < size) {
+        out[getIdx(pos,size)] = 0;
+    }
+    if (pos >= 1 && pos < size - 1) {
+        for (int ix = -1; ix <= 1; ix++) {
+            for (int iy = -1; iy <= 1; iy++) {
+                for (int iz = -1; iz <= 1; iz++) {
+                    out[getIdx(pos,size)] += in[getIdx(pos + make_int3(ix,iy,iz),size)];
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     std::string cmd;
@@ -29,8 +61,13 @@ int main(int argc, char *argv[])
         std::vector<float *> volsIn = { volIn.data() };
         std::vector<float *> volsOut = { volOut.data() };
 
-        auto nop = [](auto b, auto i, auto o, auto t){};
-        cbp::CbpResult res = cbp::blockProc(nop, volsIn, volsOut, 0, volSize, blockSize);
+        auto sum3x3x3 = [](auto b, auto s, auto in, auto out, auto tmp){
+            const int3 siz = b.blockSizeBorder();
+            const dim3 blockDim = dim3(8,8,8);
+            const dim3 gridDim = gridBlocks(blockDim, siz);
+            sum3x3x3Kernel<<<gridDim,blockDim,0,s>>>(out[0], in[0], siz);
+        };
+        cbp::CbpResult res = cbp::blockProc(sum3x3x3, volsIn, volsOut, 0, volSize, blockSize);
         if (res != cbp::CBP_SUCCESS) {
             printf("Got error!\n");
         } else {
